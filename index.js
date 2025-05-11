0a980ed80251b0623271e1672253e59c76d5515c
eyup <eyuptozcu@gmail.com>
2025-05-11 12:18:52 +0100

    fix: Correct file extension in comment for HackerNews test cases

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-// Please find the test cases, setup and teardown codes in the tests/hackerNewsTest.spec.ts file.
+// Please find the test cases, setup and teardown codes in the tests/hackerNewsTest.spec.Js file.
